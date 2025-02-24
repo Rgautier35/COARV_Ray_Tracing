c4b09e4ad1372fc759de9ea32ed060e00fad23bf
Romain <omaingautier@gmail.com>
2025-02-24 15:39:53 +0100

    Finishing chapter 1 and setting up ppm files

--- COARV_Ray_tracing/COARV_Ray_tracing.cpp
+++ COARV_Ray_tracing/COARV_Ray_tracing.cpp
@@ -1,12 +1,25 @@
+
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main()
-{
-    int nx = 200;
-    int ny = 100;
+int main() {
+    int nx = 400; // Image width
+    int ny = 200; // Image height
+    string output_name = "ch1";
 
-    cout << "P3\n" << nx << " " << ny << "\n255\n";
+    ofstream fichier("../../Images/" + output_name + ".ppm"); // Open a file in writing mode
+
+    if (!fichier) {
+        cerr << "Error : " + output_name + ".ppm  cannot be written or created" << endl;
+        return 1;
+    }
+
+    // We write the header of the ppm file (ASCII P3 format)
+    fichier << "P3\n" << nx << " " << ny << "\n255\n";
+
+    // We generate the image
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
             float r = float(i) / float(nx);
@@ -15,8 +28,14 @@ int main()
             int ir = int(255.99 * r);
             int ig = int(255.99 * g);
             int ib = int(255.99 * b);
-            cout << ir << " " << ig << " " << ib << "\n";
+
+            // We write the image with the 3 colors for each pixels in the ppm file
+            fichier << ir << " " << ig << " " << ib << "\n";
         }
     }
 
+    fichier.close(); // We close the ppm file
+
+    cout << "Image is saved under '" + output_name + ".ppm'" << endl;
+    return 0;
 }
